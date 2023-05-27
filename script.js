96e3d20e889902b7ef6b30301b17343680d27704
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-27 20:43:36 +0500

    animation About Me

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ gsap.from('.full-screen__title', {opacity:0, delay:0.5,y:30})
 gsap.from('.full-screen__text', {opacity:0, delay:1,y:-30})
 gsap.from('.full-screen__link', {opacity:0, delay:1.5,y:60})
 
+gsap.from('.about__title', {opacity:0, delay:0.5,x:30})
+gsap.from('.about__text', {opacity:0, delay:1,x:-30})
+gsap.from('.about_image', {opacity:0, delay:1.5,x:60})
+
+
+
