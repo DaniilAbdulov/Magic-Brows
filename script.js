5faa0c5b7b4e836796a92a015db2ba5100299159
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-27 20:32:45 +0500

    Portfolio

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-gsap.from('.full-screen__title', {opacity:0, delay:1,y:30})
-gsap.from('.full-screen__text', {opacity:0, delay:1.5,y:-30})
+gsap.from('.full-screen__title', {opacity:0, delay:0.5,y:30})
+gsap.from('.full-screen__text', {opacity:0, delay:1,y:-30})
+gsap.from('.full-screen__link', {opacity:0, delay:1.5,y:60})
+
