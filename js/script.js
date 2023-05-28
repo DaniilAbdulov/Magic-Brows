60b0638a9f9d81b161e6201c9f2adff543534f45
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-28 10:28:15 +0500

    wow js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ gsap.from('.full-screen__title', {opacity:0, delay:0.5,y:30})
 gsap.from('.full-screen__text', {opacity:0, delay:1,y:-30})
 gsap.from('.full-screen__link', {opacity:0, delay:1.5,y:60})
 
-gsap.from('.about__title', {opacity:0, delay:0.5,x:30})
-gsap.from('.about__text', {opacity:0, delay:1,x:-30})
-gsap.from('.about_image', {opacity:0, delay:1.5,x:60})
+
 
 
 
