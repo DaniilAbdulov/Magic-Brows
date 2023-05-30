a8e0a29a5cb0f610497df87c5c2313b5b31d402e
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-30 20:10:11 +0500

    Animation Yakor

--- js/script.js
+++ js/script.js
@@ -2,7 +2,30 @@ gsap.from('.full-screen__title', {opacity:0, delay:0.5,y:30})
 gsap.from('.full-screen__text', {opacity:0, delay:1,y:-30})
 gsap.from('.full-screen__link', {opacity:0, delay:1.5,y:60})
 
-
+var linkNav = document.querySelectorAll('[href^="#portfolio"]'),
+    V = 0.4;  // скорость, может иметь дробное значение через точку
+for (var i = 0; i < linkNav.length; i++) {
+  linkNav[i].addEventListener('click', function(e) {
+    e.preventDefault();
+    var w = window.pageYOffset,  // прокрутка
+        hash = this.href.replace(/[^#]*(.*)/, '$1');  // id элемента, к которому нужно перейти
+        t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+        start = null;
+        console.log(t)
+    requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+    function step(time) {
+      if (start === null) start = time;
+      var progress = time - start,
+          r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+      window.scrollTo(0,r);
+      if (r != w + t) {
+        requestAnimationFrame(step)
+      } else {
+        location.hash = hash  // URL с хэшем
+      }
+    }
+  }, false);
+}
 
 
 
