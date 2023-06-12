a6017820750cab366cecd1735f16547542dc2975
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-06-12 15:14:04 +0500

    php files

--- sendmail.php
+++ sendmail.php
@@ -1,46 +1,30 @@
 <?php
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer-master\src\Exception.php';
-require 'PHPMailer-master\src\PHPMailer.php';
-
-$mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8';
-$mail->setLanguage('ru', 'PHPMailer-master\language');
-$mail->IsHTML(true);
-
-$mail->setFrom('webmaster@magicbrows.ru');
-$mail->addAddress('abdulov.dany@yandex.ru');
-$mail->Subject = 'Сообщение с сайта MagicBrows';
-
-$body = '<h1>Сообщение:</h1>';
-
-
-if(trim(!empty($_POST['name']))){
-    $body.='<p><strong>Имя:</strong> ' .$_POST['name'].'</p>';
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$datetime = $_POST['datetime'];
+$name = htmlspecialchars($name);
+$phone = htmlspecialchars($phone);
+$message = htmlspecialchars($message);
+$datetime = htmlspecialchars($datetime);
+$name = urldecode($name);
+$phone = urldecode($phone);
+$message = urldecode($message);
+$datetime = urldecode($datetime);
+$name = trim($name);
+$phone = trim($phone);
+$message = trim($message);
+$datetime = trim($datetime);
+//echo $name;
+//echo "<br>";
+//echo $phone;
+//echo "<br>";
+//echo $message;
+//echo "<br>";
+//echo $datetime;
+if (mail("example@mail.ru", "Заявка с сайта", "Имя:".$name.". Телефон: ".$phone ,"Сообщение:".$message.". Дата и время: ".$datetime ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
 }
-if(trim(!empty($_POST['phone']))){
-    $body.='<p><strong>Телефон:</strong> ' .$_POST['phone'].'</p>';
-}
-if(trim(!empty($_POST['message']))){
-    $body.='<p><strong>Сообщение:</strong> ' .$_POST['message'].'</p>';
-}
-if(trim(!empty($_POST['datetime']))){
-    $body.='<p><strong>Время и дата:</strong> ' .$_POST['datetime'].'</p>';
-}
-
-
-$mail->Body = $body;
-
-if(!$mail->send()){
-    $message = 'Error';
-}else{
-    $message = 'Sended';
-}
-$response = ['message'=>$message];
-
-header('Content-type: application/json');
-echo json_encode($response);
 ?>
