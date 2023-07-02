1bce35219306e25ad81f70a9d710837630ea710d
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-07-02 12:57:24 +0500

    updates

--- js/script.js
+++ js/script.js
@@ -11,28 +11,35 @@ const loader = document.querySelector(".body__loader");
 loader.style.display = "block";
 content.style.display = "none";
 
+const question = document.querySelector(".question");
+const qButton = document.getElementById("questionButton");
+
 // Ждем загрузки всех ресурсов
 window.addEventListener("load", function () {
     loader.style.display = "none";
-    content.style.display = "block";
-    const swiper = new Swiper(".mySwiper", {
-        effect: "coverflow",
-        loop: true,
-        speed: 1000,
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        coverflowEffect: {
-            depth: 100,
-            modifier: 1,
-            rotate: 50,
-            slideShadows: false,
-            stretch: 0,
-        },
+    question.style.display = "flex";
+    qButton.addEventListener("click", function () {
+        question.style.display = "none";
+        content.style.display = "block";
+        const swiper = new Swiper(".mySwiper", {
+            effect: "coverflow",
+            loop: true,
+            speed: 1000,
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            coverflowEffect: {
+                depth: 100,
+                modifier: 1,
+                rotate: 50,
+                slideShadows: false,
+                stretch: 0,
+            },
+        });
+        new WOW().init();
+        gsap.from(".full-screen__title", { opacity: 0, delay: 0.5, y: 60 });
+        // gsap.from(".full-screen__text", { opacity: 0, delay: 1, y: -60 });
     });
-    new WOW().init();
-    gsap.from(".full-screen__title", { opacity: 0, delay: 0.5, y: 60 });
-    // gsap.from(".full-screen__text", { opacity: 0, delay: 1, y: -60 });
 });
 //время в форме
 const dateControl = document.querySelector('input[type="datetime-local"]');
