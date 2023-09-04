bebe213fe5b58566c93bd9fa0760dc48e17ea54e
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-09-04 20:27:03 +0500

    well done

--- js/script.js
+++ js/script.js
@@ -11,32 +11,25 @@ const loader = document.querySelector(".body__loader");
 loader.style.display = "block";
 content.style.display = "none";
 
-const question = document.querySelector(".question");
-const qButton = document.getElementById("questionButton");
-
 // Ждем загрузки всех ресурсов
 window.addEventListener("load", function () {
     loader.style.display = "none";
-    question.style.display = "flex";
-    qButton.addEventListener("click", function () {
-        question.style.display = "none";
-        content.style.display = "block";
-        const swiper = new Swiper(".mySwiper", {
-            effect: "coverflow",
-            loop: true,
-            speed: 1000,
-            grabCursor: true,
-            centeredSlides: true,
-            slidesPerView: "auto",
-            coverflowEffect: {
-                depth: 100,
-                modifier: 1,
-                rotate: 50,
-                slideShadows: false,
-                stretch: 0,
-            },
-        });
-        new WOW().init();
-        gsap.from(".full-screen__title", { opacity: 0, delay: 0.5, y: 60 });
+    content.style.display = "block";
+    const swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        loop: true,
+        speed: 1000,
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            depth: 100,
+            modifier: 1,
+            rotate: 50,
+            slideShadows: false,
+            stretch: 0,
+        },
     });
+    new WOW().init();
+    gsap.from(".full-screen__title", { opacity: 0, delay: 0.5, y: 60 });
 });
